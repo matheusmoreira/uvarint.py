840b2a8fe41d05565576daef429c58666e95c11b
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-12-12 09:01:03 -0300

    Define and test the limits of the decoder
    The decoder should raise an error when it encounters a large number
    if given default parameters but not if a limit of infinity is specified.
    
    The encoder does not enforce byte buffer size limitations
    because the input can be checked directly by the caller:
    
        if number >= 2 ** 63:
            raise OverflowError('number larger than limit')

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 import typing
 
@@ -17,14 +18,16 @@ class TestUvarint(unittest.TestCase):
     ]
 
     upper_bound: typing.Tuple[int, bytes] = (2 ** 63 - 1, bytes([0b1111_1111] * 8 + [0b0111_1111]))
+    over_limit: typing.Tuple[int, bytes] = (2 ** 63, bytes([0b1000_0000] * 9 + [0b0000_0001]))
 
     values: typing.List[typing.Tuple[int, bytes]] = examples + [upper_bound]
+    all: typing.List[typing.Tuple[int, bytes]] = values + [over_limit]
 
     def test_encoding(self) -> None:
         decoded: int
         encoded: bytes
 
-        for (decoded, encoded) in TestUvarint.values:
+        for (decoded, encoded) in TestUvarint.all:
             self.assertEqual(uvarint.encode(decoded), encoded)
 
     def test_decoding(self) -> None:
@@ -33,6 +36,17 @@ class TestUvarint(unittest.TestCase):
 
         for (decoded, encoded) in TestUvarint.values:
             self.assertEqual(uvarint.decode(encoded), decoded)
+
+    def test_limits(self) -> None:
+        self.assertEqual(len(TestUvarint.upper_bound[1]), 9)
+
+        decoded, encoded = TestUvarint.over_limit
+        self.assertEqual(len(encoded), 10)
+
+        self.assertEqual(uvarint.decode(encoded, max=math.inf), decoded)
+
+        with self.assertRaises(OverflowError):
+            uvarint.decode(encoded)
 
 if __name__ == '__main__':
     unittest.main()
