817d0b5d55311efd8ed1fed0a8cd887b54318cb8
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-12-12 05:35:36 -0300

    Add tests for uvarint encoding and decoding
    Use the examples from the specification.
    
    References:
        https://github.com/multiformats/unsigned-varint/blob/master/README.md

---
+++ test.py
@@ -0,0 +1,34 @@
+import unittest
+import typing
+
+import uvarint
+
+class TestUvarint(unittest.TestCase):
+
+    # Example values from the multiformats specification README
+    # https://github.com/multiformats/unsigned-varint/blob/master/README.md
+    values: typing.List[typing.Tuple[int, bytes]] = [
+        (1,     bytes([0b0000_0001])),
+        (127,   bytes([0b0111_1111])),
+        (128,   bytes([0b1000_0000, 0b0000_0001])),
+        (255,   bytes([0b1111_1111, 0b0000_0001])),
+        (300,   bytes([0b1010_1100, 0b0000_0010])),
+        (16384, bytes([0b1000_0000, 0b1000_0000, 0b0000_0001]))
+    ]
+
+    def test_encoding(self) -> None:
+        decoded: int
+        encoded: bytes
+
+        for (decoded, encoded) in TestUvarint.values:
+            self.assertEqual(uvarint.encode(decoded), encoded)
+
+    def test_decoding(self) -> None:
+        decoded: int
+        encoded: bytes
+
+        for (decoded, encoded) in TestUvarint.values:
+            self.assertEqual(uvarint.decode(encoded), decoded)
+
+if __name__ == '__main__':
+    unittest.main()
