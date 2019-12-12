31a50197a3d4d151ee4041db9070a038d3e42481
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-12-12 08:46:18 -0300

    Rename values to examples and add new variable
    The values valuable will refer to all test data
    that can be encoded and decoded with default parameters.

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestUvarint(unittest.TestCase):
 
     # Example values from the multiformats specification README
     # https://github.com/multiformats/unsigned-varint/blob/master/README.md
-    values: typing.List[typing.Tuple[int, bytes]] = [
+    examples: typing.List[typing.Tuple[int, bytes]] = [
         (1,     bytes([0b0000_0001])),
         (127,   bytes([0b0111_1111])),
         (128,   bytes([0b1000_0000, 0b0000_0001])),
@@ -15,6 +15,8 @@ class TestUvarint(unittest.TestCase):
         (300,   bytes([0b1010_1100, 0b0000_0010])),
         (16384, bytes([0b1000_0000, 0b1000_0000, 0b0000_0001]))
     ]
+
+    values: typing.List[typing.Tuple[int, bytes]] = examples
 
     def test_encoding(self) -> None:
         decoded: int
